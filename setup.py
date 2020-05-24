5d2e1b87ce566b238eee5c954e70e60e0db4ad69
avivem <19512757+avivem@users.noreply.github.com>
2020-05-24 17:22:49 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='AABot',
+    version='1.0',
+    description='A simple Discord bot written in Python using the Discord.py API',
+    author='Aviv Elazar-Mittelman',
+    author_email='avivelazarmittelman@gmail.com',
+    packages=['AABot'],
+    install_requires=['discord.py']
+)
